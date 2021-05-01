cdc60fe38392876bca88213d3182f655ab4ef774
MichaelAKuan <77208859+MichaelAKuan@users.noreply.github.com>
2021-05-01 14:00:12 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,54 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mytab.isHittingTile(CollisionDirection.Bottom) || (mytab.isHittingTile(CollisionDirection.Left) || mytab.isHittingTile(CollisionDirection.Right))) {
+        mytab.vy = -200
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mytab.setImage(leftFacingImg)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mytab.setImage(rightFacingImg)
+})
+let mytab: Sprite = null
+let leftFacingImg: Image = null
+let rightFacingImg: Image = null
 let rightSwordOutImg = img`
-    . . . . . . . f f . . . . . . . 
-    . . . . f f f f 2 f f . . . . . 
-    . . f f e e e e f 2 f f . . . . 
-    . f f e e e e e f 2 2 f f . . . 
+    8 . . . 9 . . f f . . . . 8 . . 
+    . . 8 . f f f f 9 f f . . . . 9 
+    . . f f e e e e f 9 f f . . . . 
+    . f f e e e e e f 6 6 f f . 6 . 
     . f e e e e f f e e e e f . . . 
-    . f f f f f e e 2 2 2 2 e f . . 
-    f f f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f e e 8 8 8 8 e f . . 
+    f f f e 8 8 8 f f f f e 8 f . . 
     f f f f f f f f e e e f f f . . 
-    f e f e 4 4 e b f 4 4 e e f . . 
-    . f e e 4 d 4 b f d d e f . . . 
-    . . f e e e 4 d d d e e . c . . 
-    . . . f 2 2 2 2 e e d d e c c c 
-    . . . f 4 4 4 e 4 4 d d e c d d 
-    . . . f f f f f e e e e . c c c 
-    . . f f f f f f f f . . . c . . 
-    . . f f f . . f f . . . . . . . 
+    f e f e 4 4 e 8 9 4 4 e e f . . 
+    . f e e 4 d 4 8 6 d d e f . . . 
+    . . f e e e 4 d d d e e . 9 . . 
+    . . . f 6 6 8 9 e e d d e 9 9 9 
+    . 8 . f 4 4 4 e 4 4 d d e 9 8 8 
+    . . . f f f f f e e e e . 9 9 9 
+    6 . f f f f f f f f . . . 9 . . 
+    . . f f f . . f f . . . 8 . . . 
     `
 let leftSwordOutImg = img`
     . . . . . . . f f . . . . . . . 
-    . . . . . f f 2 f f f f . . . . 
-    . . . . f f 2 f e e e e f f . . 
-    . . . f f 2 2 f e e e e e f f . 
+    . . 2 . . f f 5 f f f f . . 4 . 
+    . . . . f f 4 f e e e e f f . . 
+    . . . f f 5 2 f e e e e e f f . 
     . . . f e e e e f f e e e e f . 
-    . . f e 2 2 2 2 e e f f f f f . 
-    . . f 2 e f f f f 2 2 2 e f f f 
+    . . f e 2 4 2 2 e e f f f f f . 
+    . . f 5 e f f f f 2 4 2 e f f f 
     . . f f f e e e f f f f f f f f 
-    . . f e e 4 4 f b e 4 4 e f e f 
-    . . . f e d d f b 4 d 4 e e f . 
-    . . c . e e d d d 4 e e e f . . 
-    c c c e d d e e 2 2 2 2 f . . . 
-    d d c e d d 4 4 e 4 4 4 f . . . 
-    c c c . e e e e f f f f f . . . 
-    . . c . . . f f f f f f f f . . 
-    . . . . . . . f f . . f f f . . 
+    . . f e e 4 4 5 4 e 4 4 e f e f 
+    . . . f e d d 2 2 4 d 4 e e f . 
+    . . 2 . e e d d d 4 e e e f . . 
+    4 4 2 e d d e e 2 2 2 2 f . . . 
+    5 5 2 e d d 4 4 e 4 4 4 f . 4 . 
+    4 4 2 . e e e e f f f f f . . . 
+    . . 2 . . . f f f f f f f f . . 
+    . . . . . 2 . f f . . f f f . 2 
     `
-let rightFacingImg = img`
+rightFacingImg = img`
     . . . . . . . . . . . . . . . . 
     . . . . . f f f f f f . . . . . 
     . . . f f e e e e f 2 f . . . . 
@@ -52,7 +66,7 @@ let rightFacingImg = img`
     . . . f f f f f f f f f f . . . 
     . . . . f f . . . f f f . . . . 
     `
-let leftFacingImg = img`
+leftFacingImg = img`
     . . . . . . . . . . . . . . . . 
     . . . . . f f f f f f . . . . . 
     . . . . f 2 f e e e e f f . . . 
@@ -70,3 +84,23 @@ let leftFacingImg = img`
     . . . f f f f f f f f f f . . . 
     . . . . f f f . . . f f . . . . 
     `
+tiles.setTilemap(tilemap`level1`)
+mytab = sprites.create(leftFacingImg, SpriteKind.Player)
+scene.cameraFollowSprite(mytab)
+controller.moveSprite(mytab, 150, 0)
+tiles.placeOnTile(mytab, tiles.getTileLocation(1, 29))
+mytab.ay = 450
+game.onUpdateInterval(100, function () {
+    if (mytab.isHittingTile(CollisionDirection.Left)) {
+        mytab.ay = 0
+        mytab.vy = 0
+        mytab.setImage(leftSwordOutImg)
+    }
+    if (mytab.isHittingTile(CollisionDirection.Right)) {
+        mytab.ax = 0
+        mytab.vx = 15
+        mytab.setImage(rightSwordOutImg)
+    } else {
+        mytab.ay = 300
+    }
+})
